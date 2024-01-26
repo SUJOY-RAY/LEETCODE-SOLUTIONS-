849242e5f6813abd550fe0b46008e93cdd26c3cd
SUJOY-RAY <157723180+SUJOY-RAY@users.noreply.github.com>
2024-01-26 08:57:57 +0530

    Create Solution1.java

---
+++ Solution1.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numIndices = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int complement = target - num;
+
+            if (numIndices.containsKey(complement)) {
+                return new int[]{numIndices.get(complement), i};
+            }
+
+            numIndices.put(num, i);
+        }
+
+        return new int[0];
+    }
+}
