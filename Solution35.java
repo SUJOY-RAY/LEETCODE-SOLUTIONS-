d97b1fff9e04134a1283e03a0ebfe1c342e3efae
SUJOY-RAY <157723180+SUJOY-RAY@users.noreply.github.com>
2024-02-22 19:26:41 +0530

    35. Search Insert Position

---
+++ Solution35.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int a=0;a<nums.length;a++){
+            if(nums[a]==target){
+                return a;
+            }
+            else if(nums[a]>target){
+                return a;
+            }
+        }
+        return nums.length;
+    }
+}
