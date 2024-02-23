d28699046bcaf5c37faa37e56a22c6add6fd1ab0
SUJOY-RAY <157723180+SUJOY-RAY@users.noreply.github.com>
2024-02-23 20:03:14 +0530

    32. Longest Valid Parentheses

---
+++ Solution32.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = [-1]
+        validLen = 0
+
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if stack:
+                    validLen = max(validLen, i - stack[-1])
+                else:
+                    stack.append(i)
+
+        return validLen
+
+
