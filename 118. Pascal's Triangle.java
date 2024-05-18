97e8fda094137b2c4fe38e1d2c2906dfbe199f18
SUJOY-RAY <157723180+SUJOY-RAY@users.noreply.github.com>
2024-05-18 18:54:31 +0530

    Given an integer numRows, return the first numRows of Pascal's triangle.

--- 118. Pascal's Triangle.java
+++ 118. Pascal's Triangle.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>>res=new ArrayList<>();
-        res.add(List.of(1));
-        for(int i=0;i<numRows-1;i++){
-            List<Integer>dummyRow=new ArrayList<>();
-            dummyRow.add(0);
-            dummyRow.addAll(res.get(res.size()-1));
-            dummyRow.add(0);
-            List<Integer>Row=new ArrayList<>();
-            for(int j=0;j<dummyRow.size()-1;j++){
-                Row.add(dummyRow.get(j)+dummyRow.get(j+1));
-            }
-            res.add(Row);
+        
+        List<List<Integer>>triangle=new ArrayList<>();
 
+        for(int i=0;i<numRows;i++){
+            List<Integer> row=new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j==0||j==i){
+                    row.add(1);
+                }else{
+                    row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
+                }
+            }
+            triangle.add(row);
         }
-        return res;
+        return triangle;
+
     }
 }
