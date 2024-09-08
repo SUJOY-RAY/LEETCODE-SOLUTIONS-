633a4d9d17fcb76b8f3676e50629a2e5b7ec54d8
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2024-09-08 08:20:56 +0530

    We are playing the Guess Game. The game is as follows:  I pick a number from 1 to n. You have to guess which number I picked.  Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.  You call a pre-defined API int guess(int num), which returns three possible results:  -1: Your guess is higher than the number I picked (i.e. num > pick). 1: Your guess is lower than the number I picked (i.e. num < pick). 0: your guess is equal to the number I picked (i.e. num == pick). Return the number that I picked.     Example 1:  Input: n = 10, pick = 6 Output: 6 Example 2:  Input: n = 1, pick = 1 Output: 1 Example 3:  Input: n = 2, pick = 1 Output: 1
    We are playing the Guess Game. The game is as follows:
    
    I pick a number from 1 to n. You have to guess which number I picked.
    
    Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
    
    You call a pre-defined API int guess(int num), which returns three possible results:
    
    -1: Your guess is higher than the number I picked (i.e. num > pick).
    1: Your guess is lower than the number I picked (i.e. num < pick).
    0: your guess is equal to the number I picked (i.e. num == pick).
    Return the number that I picked.
    
     
    
    Example 1:
    
    Input: n = 10, pick = 6
    Output: 6
    Example 2:
    
    Input: n = 1, pick = 1
    Output: 1
    Example 3:
    
    Input: n = 2, pick = 1
    Output: 1

---
+++ 374. Guess Number Higher or Lower.cpp
@@ -0,0 +1,43 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        long long left=1, right=n;
+        while(left<=right){
+            long long mid=(left+right)/2;
+
+            if(guess(mid)==-1){
+                right=mid-1;
+            }else if(guess(mid)==1){
+                left=mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return n;
+    }
+};
+
+// class Solution {
+// public:
+//     int guessNumber(int n) {
+//         long long left=1, right=n;
+//         while(guess(n)!=0){
+//             n=(left+right)/2;
+//             if(guess(n)==-1){
+//                 right=n;
+//             }else if(guess(n)==1){
+//                 left=n;
+//             }
+//         }
+//         return n;
+//     }
+// };
