efc380e137d867cc526c6d2939b2057e8cf12bf2
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2024-10-31 22:14:57 +0530

    You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.  Return the number of '*' in s, excluding the '*' between each pair of '|'.  Note that each '|' will belong to exactly one pair.     Example 1:  Input: s = "l|*e*et|c**o|*de|" Output: 2 Explanation: The considered characters are underlined: "l|*e*et|c**o|*de|". The characters between the first and second '|' are excluded from the answer. Also, the characters between the third and fourth '|' are excluded from the answer. There are 2 asterisks considered. Therefore, we return 2. Example 2:  Input: s = "iamprogrammer" Output: 0 Explanation: In this example, there are no asterisks in s. Therefore, we return 0. Example 3:  Input: s = "yo|uar|e**|b|e***au|tifu|l" Output: 5 Explanation: The considered characters are underlined: "yo|uar|e**|b|e***au|tifu|l". There are 5 asterisks considered. Therefore, we return 5.
    You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
    
    Return the number of '*' in s, excluding the '*' between each pair of '|'.
    
    Note that each '|' will belong to exactly one pair.
    
     
    
    Example 1:
    
    Input: s = "l|*e*et|c**o|*de|"
    Output: 2
    Explanation: The considered characters are underlined: "l|*e*et|c**o|*de|".
    The characters between the first and second '|' are excluded from the answer.
    Also, the characters between the third and fourth '|' are excluded from the answer.
    There are 2 asterisks considered. Therefore, we return 2.
    Example 2:
    
    Input: s = "iamprogrammer"
    Output: 0
    Explanation: In this example, there are no asterisks in s. Therefore, we return 0.
    Example 3:
    
    Input: s = "yo|uar|e**|b|e***au|tifu|l"
    Output: 5
    Explanation: The considered characters are underlined: "yo|uar|e**|b|e***au|tifu|l". There are 5 asterisks considered. Therefore, we return 5.

---
+++ 2315. Count Asterisks.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countAsterisks(string s) {
+        int count=0;
+        bool inBar=false;
+
+        for(char c:s){
+            if(c=='|'){
+                inBar=!inBar;
+            }else if(!inBar && c=='*'){
+                count++;
+            }
+        }
+        return count;
+    }
+};
