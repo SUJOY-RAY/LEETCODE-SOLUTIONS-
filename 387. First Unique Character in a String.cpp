77fbaaa5563eb26e276adb8767b2046309cac20d
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2024-11-06 20:46:31 +0530

    Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.     Example 1:  Input: s = "leetcode"  Output: 0  Explanation:  The character 'l' at index 0 is the first character that does not occur at any other index.  Example 2:  Input: s = "loveleetcode"  Output: 2  Example 3:  Input: s = "aabb"  Output: -1     Constraints:  1 <= s.length <= 105 s consists of only lowercase English letters.
    Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
    
    Example 1:
    
    Input: s = "leetcode"
    
    Output: 0
    
    Explanation:
    
    The character 'l' at index 0 is the first character that does not occur at any other index.
    
    Example 2:
    
    Input: s = "loveleetcode"
    
    Output: 2
    
    Example 3:
    
    Input: s = "aabb"
    
    Output: -1
    
     
    
    Constraints:
    
    1 <= s.length <= 105
    s consists of only lowercase English letters.

---
+++ 387. First Unique Character in a String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int counter[26]={0};
+        for(char a:s){
+            counter[a-'a']++;
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(counter[s[i]-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
