2c8c77333bbf2eca3b66d1f21bbda438a84e1824
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2024-11-14 23:25:31 +0530

    Given an integer array nums and an integer k, return the number of subarrays of nums where the least common multiple of the subarray's elements is k.  A subarray is a contiguous non-empty sequence of elements within an array.  The least common multiple of an array is the smallest positive integer that is divisible by all the array elements.     Example 1:  Input: nums = [3,6,2,7,1], k = 6 Output: 4 Explanation: The subarrays of nums where 6 is the least common multiple of all the subarray's elements are: - [3,6,2,7,1] - [3,6,2,7,1] - [3,6,2,7,1] - [3,6,2,7,1] Example 2:  Input: nums = [3], k = 2 Output: 0 Explanation: There are no subarrays of nums where 2 is the least common multiple of all the subarray's elements.
    Given an integer array nums and an integer k, return the number of subarrays of nums where the least common multiple of the subarray's elements is k.
    
    A subarray is a contiguous non-empty sequence of elements within an array.
    
    The least common multiple of an array is the smallest positive integer that is divisible by all the array elements.
    
     
    
    Example 1:
    
    Input: nums = [3,6,2,7,1], k = 6
    Output: 4
    Explanation: The subarrays of nums where 6 is the least common multiple of all the subarray's elements are:
    - [3,6,2,7,1]
    - [3,6,2,7,1]
    - [3,6,2,7,1]
    - [3,6,2,7,1]
    Example 2:
    
    Input: nums = [3], k = 2
    Output: 0
    Explanation: There are no subarrays of nums where 2 is the least common multiple of all the subarray's elements.

---
+++ 2470. Number of Subarrays With LCM Equal to K.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+   int subarrayLCM(vector<int>& nums, int k) {
+    int res = 0;
+    unordered_map<int, int> m;
+    for (int n : nums) {
+        unordered_map<int, int> m1;
+        if (k % n == 0) {
+            ++m[n];    
+            for (auto &[d, cnt] : m)
+                m1[lcm(d, n)] += cnt;
+            res += m1[k];
+        }
+        swap(m, m1);
+    }
+    return res;
+}
+};
