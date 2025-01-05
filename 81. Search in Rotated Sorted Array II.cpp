ee20f26c79a78bbf5edc9f2e9dc95293e888f537
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2025-01-05 19:17:31 +0530

    There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).  Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].  Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.  You must decrease the overall operation steps as much as possible.     Example 1:  Input: nums = [2,5,6,0,0,1,2], target = 0 Output: true Example 2:  Input: nums = [2,5,6,0,0,1,2], target = 3 Output: false    Constraints:  1 <= nums.length <= 5000 -104 <= nums[i] <= 104 nums is guaranteed to be rotated at some pivot. -104 <= target <= 104    Follow up: This problem is similar to Search in Rotated Sorted Array, but nums may contain duplicates. Would this affect the runtime complexity? How and why?
    There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
    
    Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
    
    Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
    
    You must decrease the overall operation steps as much as possible.
    
     
    
    Example 1:
    
    Input: nums = [2,5,6,0,0,1,2], target = 0
    Output: true
    Example 2:
    
    Input: nums = [2,5,6,0,0,1,2], target = 3
    Output: false
     
    
    Constraints:
    
    1 <= nums.length <= 5000
    -104 <= nums[i] <= 104
    nums is guaranteed to be rotated at some pivot.
    -104 <= target <= 104
     
    
    Follow up: This problem is similar to Search in Rotated Sorted Array, but nums may contain duplicates. Would this affect the runtime complexity? How and why?

---
+++ 81. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] == target) return true;
+
+            while (l < mid && nums[l] == nums[mid]) l++;
+            while (r > mid && nums[r] == nums[mid]) r--;
+
+            if (nums[l] <= nums[mid]) { 
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else { 
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+
+
+/*
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+                continue;
+            }
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && nums[mid] >= target) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && nums[high] >= target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+};
+*/
