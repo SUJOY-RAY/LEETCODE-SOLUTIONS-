d8ec66d0082b6b9ddf3c1ba1c2384fff996c857f
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2025-01-18 11:40:17 +0530

    Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.  If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.     Example 1:  Input: s = "abcdefg", k = 2 Output: "bacdfeg" Example 2:  Input: s = "abcd", k = 2 Output: "bacd"
    Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
    
    If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
    
     
    
    Example 1:
    
    Input: s = "abcdefg", k = 2
    Output: "bacdfeg"
    Example 2:
    
    Input: s = "abcd", k = 2
    Output: "bacd"

--- 541. Reverse String II.cpp
+++ 541. Reverse String II.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string reverseStr(string s, int k) {
-        for(int i=0;i<s.size()-k;i+=2*k){
+        for(int i=0;i<s.size();i+=2*k){
             int left=i;
             int right=min(i+k-1, (int)s.size()-1);
             while(left<right){
