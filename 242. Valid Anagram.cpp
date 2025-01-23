5019ae88d179fad8f40af98281119693c4c5aa22
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2025-01-23 10:49:42 +0530

    Given two strings s and t, return true if t is an  anagram  of s, and false otherwise.     Example 1:  Input: s = "anagram", t = "nagaram"  Output: true  Example 2:  Input: s = "rat", t = "car"  Output: false     Constraints:  1 <= s.length, t.length <= 5 * 104 s and t consist of lowercase English letters.    Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
    Given two strings s and t, return true if t is an 
    anagram
     of s, and false otherwise.
    
     
    
    Example 1:
    
    Input: s = "anagram", t = "nagaram"
    
    Output: true
    
    Example 2:
    
    Input: s = "rat", t = "car"
    
    Output: false
    
     
    
    Constraints:
    
    1 <= s.length, t.length <= 5 * 104
    s and t consist of lowercase English letters.
     
    
    Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        map<char,int> charCountS;
-        map<char,int> charCountT;
-        if(s.size()!=t.size()){
-            return false;
-        }
-        for(int i=0;i<t.size();i++){
-            charCountS[s[i]]++;
-            charCountT[t[i]]++;
-        }
-        return charCountS==charCountT;
+    // //Sorting method
+    // bool isAnagram(string s, string t) {
+    //     sort(s.begin(),s.end());
+    //     sort(t.begin(),t.end());
+    //     return s==t;
+    // }
 
+    // //Hashmap method
+    // bool isAnagram(string s, string t) {
+    //     if (s.length() != t.length()) return false;
+    //     map<char, int> countMap;
+    //     for (char a : s) {
+    //         countMap[a]++;
+    //     }
+    //     for (char c : t) {
+    //         if (countMap.find(c) == countMap.end())
+    //             return false;
+    //         countMap[c]--;
+    //         if (countMap[c] < 0)
+    //             return false;
+    //     }
+    //     return true;
+    // }
+
+    // array method
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        int countArr[26] = {0};
+        for (int i = 0; i < s.length(); i++) {
+            countArr[s[i] - 'a']++;
+            countArr[t[i] - 'a']--;
+        }
+        for (int a : countArr) {
+            if (a != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 };
