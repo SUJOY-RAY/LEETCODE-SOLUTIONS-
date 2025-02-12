858e752f94910dd0103b426498a01f8c67f75a91
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2025-02-12 17:58:51 +0530

    You are given a 0-indexed array nums consisting of positive integers. You can choose two indices i and j, such that i != j, and the sum of digits of the number nums[i] is equal to that of nums[j].  Return the maximum value of nums[i] + nums[j] that you can obtain over all possible indices i and j that satisfy the conditions.     Example 1:  Input: nums = [18,43,36,13,7] Output: 54 Explanation: The pairs (i, j) that satisfy the conditions are: - (0, 2), both numbers have a sum of digits equal to 9, and their sum is 18 + 36 = 54. - (1, 4), both numbers have a sum of digits equal to 7, and their sum is 43 + 7 = 50. So the maximum sum that we can obtain is 54. Example 2:  Input: nums = [10,12,19,14] Output: -1 Explanation: There are no two numbers that satisfy the conditions, so we return -1.    Constraints:  1 <= nums.length <= 105 1 <= nums[i] <= 109`
    You are given a 0-indexed array nums consisting of positive integers. You can choose two indices i and j, such that i != j, and the sum of digits of the number nums[i] is equal to that of nums[j].
    
    Return the maximum value of nums[i] + nums[j] that you can obtain over all possible indices i and j that satisfy the conditions.
    
     
    
    Example 1:
    
    Input: nums = [18,43,36,13,7]
    Output: 54
    Explanation: The pairs (i, j) that satisfy the conditions are:
    - (0, 2), both numbers have a sum of digits equal to 9, and their sum is 18 + 36 = 54.
    - (1, 4), both numbers have a sum of digits equal to 7, and their sum is 43 + 7 = 50.
    So the maximum sum that we can obtain is 54.
    Example 2:
    
    Input: nums = [10,12,19,14]
    Output: -1
    Explanation: There are no two numbers that satisfy the conditions, so we return -1.
     
    
    Constraints:
    
    1 <= nums.length <= 10^5
    1 <= nums[i] <= 10^9

---
+++ 2342. Max Sum of a Pair With Equal Sum of Digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumSum(int[] nums) {
+        int []max=new int[82];
+        int ans=-1;
+        for(int num:nums){
+            int sum=0;
+            int temp=num;
+            while(temp!=0){
+                sum+=temp%10;
+                temp/=10;
+            }
+            if(max[sum]!=0){
+                ans=Math.max(ans, num+max[sum]);
+            }
+            max[sum]=Math.max(max[sum],num);
+        }
+        return ans;
+    }
+}
