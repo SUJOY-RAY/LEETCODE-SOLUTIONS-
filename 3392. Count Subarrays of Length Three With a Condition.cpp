09ff65ac5b3f40836841463f07158d24ba7ee546
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2025-04-27 20:51:19 +0530

    Given an integer array nums, return the number of subarrays of length 3 such that the sum of the first and third numbers equals exactly half of the second number.     Example 1:  Input: nums = [1,2,1,4,1]  Output: 1  Explanation:  Only the subarray [1,4,1] contains exactly 3 elements where the sum of the first and third numbers equals half the middle number.  Example 2:  Input: nums = [1,1,1]  Output: 0  Explanation:  [1,1,1] is the only subarray of length 3. However, its first and third numbers do not add to half the middle number.     Constraints:  3 <= nums.length <= 100 -100 <= nums[i] <= 100
    Given an integer array nums, return the number of subarrays of length 3 such that the sum of the first and third numbers equals exactly half of the second number.
    
     
    
    Example 1:
    
    Input: nums = [1,2,1,4,1]
    
    Output: 1
    
    Explanation:
    
    Only the subarray [1,4,1] contains exactly 3 elements where the sum of the first and third numbers equals half the middle number.
    
    Example 2:
    
    Input: nums = [1,1,1]
    
    Output: 0
    
    Explanation:
    
    [1,1,1] is the only subarray of length 3. However, its first and third numbers do not add to half the middle number.
    
     
    
    Constraints:
    
    3 <= nums.length <= 100
    -100 <= nums[i] <= 100

--- 3392. Count Subarrays of Length Three With a Condition.cpp
+++ 3392. Count Subarrays of Length Three With a Condition.cpp
@@ -4,9 +4,10 @@ public:
         int counter = 0, n = nums.size();
 
         for (int i = 0; i < n - 2; i++) {
-            if (2 * (nums[i] + nums[i + 2]) == nums[i + 1]) {
-                counter++;
-            }
+            // if (2 * (nums[i] + nums[i + 2]) == nums[i + 1]) {
+            //     counter++;
+            // }
+            counter += 2*(nums[i] + nums[i+2]) == nums[i+1];
         }
         return counter;
     }
