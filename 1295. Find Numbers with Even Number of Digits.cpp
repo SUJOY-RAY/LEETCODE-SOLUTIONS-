d9bb4cb8a6c708f6a7c4e478798a62cee3dc8a49
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2025-04-30 22:14:04 +0530

    Given an array nums of integers, return how many of them contain an even number of digits.     Example 1:  Input: nums = [12,345,2,6,7896] Output: 2 Explanation:  12 contains 2 digits (even number of digits).  345 contains 3 digits (odd number of digits).  2 contains 1 digit (odd number of digits).  6 contains 1 digit (odd number of digits).  7896 contains 4 digits (even number of digits).  Therefore only 12 and 7896 contain an even number of digits. Example 2:  Input: nums = [555,901,482,1771] Output: 1  Explanation:  Only 1771 contains an even number of digits.    Constraints:  1 <= nums.length <= 500 1 <= nums[i] <= 10^5
    Given an array nums of integers, return how many of them contain an even number of digits.
    
     
    
    Example 1:
    
    Input: nums = [12,345,2,6,7896]
    Output: 2
    Explanation: 
    12 contains 2 digits (even number of digits). 
    345 contains 3 digits (odd number of digits). 
    2 contains 1 digit (odd number of digits). 
    6 contains 1 digit (odd number of digits). 
    7896 contains 4 digits (even number of digits). 
    Therefore only 12 and 7896 contain an even number of digits.
    Example 2:
    
    Input: nums = [555,901,482,1771]
    Output: 1 
    Explanation: 
    Only 1771 contains an even number of digits.
     
    
    Constraints:
    
    1 <= nums.length <= 500
    1 <= nums[i] <= 10^5

--- 1295. Find Numbers with Even Number of Digits.cpp
+++ 1295. Find Numbers with Even Number of Digits.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     int findNumbers(vector<int>& nums) {
-        int counter = 0;
-
-        for (int num : nums) {
-            int digits = log10(num) + 1;
-            if (digits % 2 == 0) {
-                counter++;
+        int result = 0;
+        for(int num:nums){
+            int count = 0;
+            while(num!=0){
+                num/=10;
+                count++;
             }
-        }
-        return counter;
+            if((count&1)==0){
+                result++;
+            }
+        }   
+        return result;
     }
 };
+
+// class Solution {
+// public:
+//     int findNumbers(vector<int>& nums) {
+//         int counter = 0;
+
+//         for (int num : nums) {
+//             int digits = log10(num) + 1;
+//             if (digits % 2 == 0) {
+//                 counter++;
+//             }
+//         }
+//         return counter;
+//     }
+// };
