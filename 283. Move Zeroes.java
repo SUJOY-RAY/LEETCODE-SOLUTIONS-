a276c420650f527fb0eea91db3ae151d489de849
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2025-09-05 10:25:31 +0530

    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.  Note that you must do this in-place without making a copy of the array.     Example 1:  Input: nums = [0,1,0,3,12] Output: [1,3,12,0,0] Example 2:  Input: nums = [0] Output: [0]    Constraints:  1 <= nums.length <= 104 -231 <= nums[i] <= 231 - 1
    Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
    
    Note that you must do this in-place without making a copy of the array.
    
     
    
    Example 1:
    
    Input: nums = [0,1,0,3,12]
    Output: [1,3,12,0,0]
    Example 2:
    
    Input: nums = [0]
    Output: [0]
     
    
    Constraints:
    
    1 <= nums.length <= 104
    -231 <= nums[i] <= 231 - 1

--- 283. Move Zeroes.java
+++ 283. Move Zeroes.java
@@ -1,15 +1,27 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-        int nonZeroIndex = 0;
+        int j = 0;
+
+        //// Naive approach [without inplace]
+        // int[] result = new int [n];
+        // Arrays.fill(result, 0);
+        // for(int i = 0; i < n; i++){
+        //     if(nums[i]!=0){
+        //         result[j++] = nums[i];
+        //     }
+        // }
+        // nums = result;
+
+        //// Inplace [2 pointer]
+
         for (int i = 0; i < n; i++) {
             if (nums[i] != 0) {
-                nums[nonZeroIndex] = nums[i];
-                nonZeroIndex++;
+                nums[j++] = nums[i];
             }
         }
-        for (int i = nonZeroIndex; i < n; i++) {
-            nums[i] = 0;
+        while(j<n){
+            nums[j++] = 0;
         }
     }
 }
