612bb714fe87b8396d7fd85a24bb85fac704de19
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2026-05-14 21:40:18 +0530

    Implement isGood method to check array validity

---
+++ 2784. Check if Array is Good.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        int n = nums.length - 1;
+
+        int[] frequencies = new int[n];
+
+        for(int number: nums) {
+            if (number < 1 || number > n) {
+                return false;
+            }
+
+            frequencies[number - 1]++;
+        } 
+
+        for(int i = 0; i < n - 1; ++i) {
+            if (frequencies[i] != 1) {
+                return false;
+            }
+        }
+
+        if (frequencies[n - 1] != 2) {
+            return false;
+        }
+
+        return true;
+    }
+}
